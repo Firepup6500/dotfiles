ded5e4b887d66ba83fcc379a2f06aa436f1e0499
Firepup Sixfifty <firepyp650@gmail.com>
2024-11-18 00:22:44 -0600

    add vscodium settings file

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,7 +1,9 @@
 mv /home/$(whoami)/.config/i3/config /home/$(whoami)/.config/i3/config-old
 mv /home/$(whoami)/.config/i3status/config /home/$(whoami)/.config/i3status/config-old
 mv /home/$(whoami)/.config/qt5ct /home/$(whoami)/.config/qt5ct-old
-mkdir -p /home/$(whoami)/.config/i3/ /home/$(whoami)/.config/i3status/
+mv /home/$(whoami)/.config/VSCodium/User/settings.json /home/$(whoami)/.config/VSCodium/User/settings-old.json
+mkdir -p /home/$(whoami)/.config/i3/ /home/$(whoami)/.config/i3status/ /home/$(whoami)/.config/VSCodium/User/
 ln -s $(pwd)/i3-config /home/$(whoami)/.config/i3/config
 ln -s $(pwd)/i3status-config /home/$(whoami)/.config/i3status/config
 ln -s $(pwd)/qt5ct /home/$(whoami)/.config/qt5ct
+ln -s $(pwd)/vscodium-settings.json /home/$(whoami)/.config/VSCodium/User/settings.json
